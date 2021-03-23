c185ea185de8c085dba54e1ccce29d6882aa7565
Kliklou34 <74457153+Kliklou34@users.noreply.github.com>
2021-03-23 21:15:28 +0100

    Add files via upload

--- champion_select.js
+++ champion_select.js
@@ -4,25 +4,18 @@ let n = Math.sqrt(154)
 
 const game=document.getElementById("game");
 
-for (let i=0;i<7;i++){
-    for (let j=1;j<22;j++){
-        let b = document.createElement("input");
-        b.setAttribute("type", "image");
-        b.setAttribute("id", (j +(i*22)));
-        b.setAttribute("alt" ,"Login");
-        b.setAttribute("src" ,"Champions - League of Legends_fichiers/("+(j +(i*22))+").jpg");
-        b.addEventListener("click", react);
-        game.appendChild(b);
-    } 
-    game.appendChild(document.createElement("br")); 
+for (let i=1;i<155;i++){
+
+    let b = document.createElement("input");
+    b.setAttribute("type", "image");
+    b.setAttribute("id", i);
+    b.setAttribute("alt" ,"Login");
+    b.setAttribute("src" ,"Champions - League of Legends_fichiers/ ("+i+").jpg");
+    b.addEventListener("click", react);
+    game.appendChild(b);
+
+
 }
-let b = document.createElement("input");
-b.setAttribute("type", "image");
-b.setAttribute("id", 154);
-b.setAttribute("alt" ,"Login");
-b.setAttribute("src" ,"Champions - League of Legends_fichiers/(154).jpg");
-b.addEventListener("click", react);
-game.appendChild(b);
 
 function react(){
     
--- page_principale.js
+++ page_principale.js
@@ -6,15 +6,6 @@ document.getElementById("icone3").addEventListener("click", react3);
 document.getElementById("icone4").addEventListener("click", react4);
 document.getElementById("icone5").addEventListener("click", react5);
 
-if (localStorage.getItem('id_icone1') == '0'){
-    for (let i=1;i<6;i++){
-        
-        localStorage.setItem('id_icone'+i, 0);
-        console.log(i,localStorage.getItem('id_icone'+i))
-        alert(i,localStorage.getItem('id_icone'+i))
-    }
-}
-
 function react1(){
     localStorage.setItem('joueur_id',1);
 
@@ -36,10 +27,23 @@ if (localStorage.getItem('id_champ_selct')!= 'None' ){
     
     for (let i=1;i<6;i++){
         let marty = document.getElementById('icone_player_'+i)
-        
-        marty.setAttribute("src" ,"Champions - League of Legends_fichiers/("+localStorage.getItem('id_icone'+i)+").jpg");
+        marty.setAttribute("src" ,"Champions - League of Legends_fichiers/ ("+localStorage.getItem('id_icone'+i)+").jpg");
     }
 }
 
 localStorage.setItem('id_champ_selct', 'None');
+
+let caribou = []
+let requestURL = 'http://ddragon.leagueoflegends.com/cdn/11.6.1/data/en_US/champion.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    for (let [cle] of Object.entries(request.response.data)){
+        caribou.push(`${String(cle)}`)
+      }
+      console.log((request.response.data[caribou[0]]).key)
+  }
+
 
